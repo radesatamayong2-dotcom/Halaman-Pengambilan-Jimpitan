2ebe336e82d681a7dbb1ef3bbf7106c0b994cdfd
radesatamayong2-dotcom <radesatamayong2@gmail.com>
2026-02-17 22:38:58 +0700

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase config
+export const firebaseConfig = {
+  apiKey: "AIzaSyAZIVBo5z2Vz7Vp8i8-ZZTkAhdsKrU5e9Q",
+  authDomain: "iuran-warga-rt-02-rw-03-c14fa.firebaseapp.com",
+  databaseURL: "https://iuran-warga-rt-02-rw-03-c14fa-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "iuran-warga-rt-02-rw-03-c14fa",
+  storageBucket: "iuran-warga-rt-02-rw-03-c14fa.firebasedatabase.app",
+  messagingSenderId: "717148916230",
+  appId: "1:717148916230:web:22981ac3c7df301d4e5187"
+};
